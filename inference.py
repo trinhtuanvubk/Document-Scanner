7f90ffe76df3da8dd3215f91a451e8874bddfb9d
vutt <vutuantrinh2000@gmail.com>
2023-12-15 03:57:48 +0000

    init

---
+++ inference.py
@@ -0,0 +1,244 @@
+import os
+import gc
+import io
+import cv2
+import base64
+import pathlib
+import numpy as np
+from PIL import Image
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+import torch
+import torchvision.transforms as torchvision_T
+from torchvision.models.segmentation import deeplabv3_resnet50, deeplabv3_mobilenet_v3_large
+
+def load_model(num_classes=2, model_name="mbv3", device=torch.device("cpu")):
+    if model_name == "mbv3":
+        model = deeplabv3_mobilenet_v3_large(num_classes=num_classes, aux_loss=True)
+        checkpoint_path = os.path.join(os.getcwd(), "model_repository", "model_mbv3_iou_mix_2C049.pth")
+    else:
+        model = deeplabv3_resnet50(num_classes=num_classes, aux_loss=True)
+        checkpoint_path = os.path.join(os.getcwd(), "model_repository", "model_r50_iou_mix_2C020.pth")
+
+    model.to(device)
+    checkpoints = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoints, strict=False)
+    model.eval()
+
+    _ = model(torch.randn((1, 3, 384, 384)))
+
+    return model
+
+def image_preprocess_transforms(mean=(0.4611, 0.4359, 0.3905), std=(0.2193, 0.2150, 0.2109)):
+    common_transforms = torchvision_T.Compose(
+        [
+            torchvision_T.ToTensor(),
+            torchvision_T.Normalize(mean, std),
+        ]
+    )
+    return common_transforms
+
+
+
+def order_points(pts):
+    """Rearrange coordinates to order:
+    top-left, top-right, bottom-right, bottom-left"""
+    rect = np.zeros((4, 2), dtype="float32")
+    pts = np.array(pts)
+    s = pts.sum(axis=1)
+    # Top-left point will have the smallest sum.
+    rect[0] = pts[np.argmin(s)]
+    # Bottom-right point will have the largest sum.
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    # Top-right point will have the smallest difference.
+    rect[1] = pts[np.argmin(diff)]
+    # Bottom-left will have the largest difference.
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect.astype("int").tolist()
+
+
+def find_dest(pts):
+    (tl, tr, br, bl) = pts
+    # Finding the maximum width.
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # Finding the maximum height.
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # Final destination co-ordinates.
+    destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]]
+
+    return order_points(destination_corners)
+
+
+def scan(image_true=None, trained_model=None, image_size=384, BUFFER=10):
+    global preprocess_transforms
+
+    IMAGE_SIZE = image_size
+    half = IMAGE_SIZE // 2
+
+    imH, imW, C = image_true.shape
+
+    image_model = cv2.resize(image_true, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
+
+    scale_x = imW / IMAGE_SIZE
+    scale_y = imH / IMAGE_SIZE
+
+    image_model = preprocess_transforms(image_model)
+    print(image_model.shape)
+    image_model = torch.unsqueeze(image_model, dim=0)
+
+    with torch.no_grad():
+        out = trained_model(image_model)["out"].cpu()
+
+    del image_model
+    gc.collect()
+
+    out = torch.argmax(out, dim=1, keepdims=True).permute(0, 2, 3, 1)[0].numpy().squeeze().astype(np.int32)
+    r_H, r_W = out.shape
+
+    _out_extended = np.zeros((IMAGE_SIZE + r_H, IMAGE_SIZE + r_W), dtype=out.dtype)
+    _out_extended[half : half + IMAGE_SIZE, half : half + IMAGE_SIZE] = out * 255
+    out = _out_extended.copy()
+
+    del _out_extended
+    gc.collect()
+
+    # Edge Detection.
+    canny = cv2.Canny(out.astype(np.uint8), 225, 255)
+    canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+    contours, _ = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    page = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+
+    # ==========================================
+    epsilon = 0.02 * cv2.arcLength(page, True)
+    corners = cv2.approxPolyDP(page, epsilon, True)
+
+    corners = np.concatenate(corners).astype(np.float32)
+
+    corners[:, 0] -= half
+    corners[:, 1] -= half
+
+    corners[:, 0] *= scale_x
+    corners[:, 1] *= scale_y
+
+    # check if corners are inside.
+    # if not find smallest enclosing box, expand_image then extract document
+    # else extract document
+
+    if not (np.all(corners.min(axis=0) >= (0, 0)) and np.all(corners.max(axis=0) <= (imW, imH))):
+
+        left_pad, top_pad, right_pad, bottom_pad = 10, 10, 10, 10
+
+        rect = cv2.minAreaRect(corners.reshape((-1, 1, 2)))
+        box = cv2.boxPoints(rect)
+        box_corners = np.int32(box)
+        #     box_corners = minimum_bounding_rectangle(corners)
+
+        box_x_min = np.min(box_corners[:, 0])
+        box_x_max = np.max(box_corners[:, 0])
+        box_y_min = np.min(box_corners[:, 1])
+        box_y_max = np.max(box_corners[:, 1])
+
+        # Find corner point which doesn't satify the image constraint
+        # and record the amount of shift required to make the box
+        # corner satisfy the constraint
+        if box_x_min <= 0:
+            left_pad = abs(box_x_min) + BUFFER
+
+        if box_x_max >= imW:
+            right_pad = (box_x_max - imW) + BUFFER
+
+        if box_y_min <= 0:
+            top_pad = abs(box_y_min) + BUFFER
+
+        if box_y_max >= imH:
+            bottom_pad = (box_y_max - imH) + BUFFER
+
+        # new image with additional zeros pixels
+        image_extended = np.zeros((top_pad + bottom_pad + imH, left_pad + right_pad + imW, C), dtype=image_true.dtype)
+
+        # adjust original image within the new 'image_extended'
+        image_extended[top_pad : top_pad + imH, left_pad : left_pad + imW, :] = image_true
+        image_extended = image_extended.astype(np.float32)
+
+        # shifting 'box_corners' the required amount
+        box_corners[:, 0] += left_pad
+        box_corners[:, 1] += top_pad
+
+        corners = box_corners
+        image_true = image_extended
+
+    corners = sorted(corners.tolist())
+    corners = order_points(corners)
+    destination_corners = find_dest(corners)
+    M = cv2.getPerspectiveTransform(np.float32(corners), np.float32(destination_corners))
+
+    final = cv2.warpPerspective(image_true, M, (destination_corners[2][0], destination_corners[2][1]), flags=cv2.INTER_LANCZOS4)
+    final = np.clip(final, a_min=0, a_max=255)
+    final = final.astype(np.uint8)
+
+    return final
+
+
+def apply_enhancements(image):
+    # Apply contrast enhancement using CLAHE (Contrast Limited Adaptive Histogram Equalization)
+    lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab_image)
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    enhanced_l = clahe.apply(l)
+    enhanced_lab = cv2.merge([enhanced_l, a, b])
+    enhanced_image = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
+
+    # Apply other enhancements as needed
+    # You can experiment with techniques like smoothing, sharpening, etc.
+
+    return enhanced_image
+
+def enhance_text_filter2D(image_path):
+    # Read the image
+    original_image = cv2.imread(image_path)
+
+    # Check if the image is loaded successfully
+    if original_image is None:
+        print(f"Error: Unable to load image from {image_path}")
+        return
+
+    # Define a sharpening kernel
+    sharpening_kernel = np.array([[-1, -1, -1],
+                                  [-1, 9, -1],
+                                  [-1, -1, -1]])
+
+    # Apply the sharpening kernel using filter2D
+    enhanced_image = cv2.filter2D(original_image, -1, sharpening_kernel)
+    return enhanced_image
+
+
+if __name__ == "__main__":
+    data_path = "./Epay_AI"
+    output_path = "./Epay_Output"
+    os.makedirs(output_path, exist_ok=True)
+    IMAGE_SIZE = 384
+    # model = load_model(model_name="mbv3")
+    preprocess_transforms = image_preprocess_transforms()
+    model = load_model(model_name="r50")
+    for file in os.listdir(data_path):
+        filepath = os.path.join(data_path, file)
+        outfile_path = os.path.join(output_path, file)
+        # print(filepath)
+        print(output_path)
+        image = cv2.imread(filepath)
+        h, w = image.shape[:2]
+        final = scan(image_true=image, trained_model=model, image_size=IMAGE_SIZE)
+        # final = apply_enhancements(final)
+        cv2.imwrite(outfile_path, final)
+
+
+
